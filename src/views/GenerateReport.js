b08ce11d815ba9335dbc6d841e049ad586542f4d
Kai Vandivier <kpvandivier@gmail.com>
2020-08-28 16:13:43 -0700

    fix: typo

--- src/views/GenerateReport.js
+++ src/views/GenerateReport.js
@@ -2,5 +2,5 @@ import React from 'react'
 import i18n from '../locales'
 
 export const GenerateReport = () => (
-    <h1>{i18n.t('Gereate Report from Custom Template')}</h1>
+    <h1>{i18n.t('Generate Report from Custom Template')}</h1>
 )
