40ef3650331e21afdd32bc0ddf7c66b78b4d0515
Kai Vandivier <kpvandivier@gmail.com>
2020-08-31 15:31:53 -0700

    style: add styles from `d2/analytics`

---
+++ src/pages/table-template/DataSelector/styles/DataTypesSelector.style.js
@@ -0,0 +1,10 @@
+import css from 'styled-jsx/css'
+
+export default css`
+    .container {
+        display: flex;
+        flex-flow: column;
+        border-bottom: 0;
+        padding: 5px 5px 10px 10px;
+    }
+`
---
+++ src/pages/table-template/DataSelector/styles/Detail.style.js
@@ -0,0 +1,9 @@
+import css from 'styled-jsx/css'
+
+export default css`
+    .detail-container {
+        display: flex;
+        flex-flow: column;
+        padding-left: 6px;
+    }
+`
