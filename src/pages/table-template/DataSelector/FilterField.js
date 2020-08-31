6c39999d1caf34defe774a5f988459ca0e78a02b
Kai Vandivier <kpvandivier@gmail.com>
2020-08-31 16:23:38 -0700

    feat: copy in FilterField from `analytics`

---
+++ src/pages/table-template/DataSelector/FilterField.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import i18n from '@dhis2/d2-i18n'
+
+import Filter from './Filter/Filter'
+import styles from './styles/FilterField.style'
+
+export const FilterField = ({ text, onFilterTextChange, onClearFilter }) => (
+    <div className="container">
+        <Filter
+            placeholder={i18n.t('Search')}
+            text={text}
+            onChange={onFilterTextChange}
+            onClear={onClearFilter}
+        />
+        <style jsx>{styles}</style>
+    </div>
+)
+
+FilterField.propTypes = {
+    text: PropTypes.string.isRequired,
+    onClearFilter: PropTypes.func.isRequired,
+    onFilterTextChange: PropTypes.func.isRequired,
+}
+
+export default FilterField
