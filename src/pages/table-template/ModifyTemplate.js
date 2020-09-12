18e8e69faed2718c68bca4182c09f475d51619e3
Kai Vandivier <kpvandivier@gmail.com>
2020-09-12 14:40:28 -0700

    feat: add first templating based on table state

--- src/pages/table-template/ModifyTemplate.js
+++ src/pages/table-template/ModifyTemplate.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import {
     Button,
     ButtonStrip,
@@ -13,27 +13,61 @@ import {
 import i18n from '../../locales'
 
 import { DataEntryCell } from './DataEntryCell'
+import tableReducer from '../../reducers/tableReducer'
+// Testing purposes (TODO: Remove when functional)
+import testTable from '../../modules/testTable'
 
-export const TemplatingTable = () => {
+// TODO:
+// DONE - Apply reducer to manage table state
+// DONE - Template out table components from state
+// Give subcomponents `dispatch` functionality to modify state (import action types)
+// (add row/column, update+reorder+delete row/column, update cell)
+// create `add row/column` modals
+// create controls to update/reorder/delete on each row and column
+
+export function TemplatingTable() {
+    const [table, dispatch] = useReducer(tableReducer, testTable)
+
+    function tableColumns() {
+        // TODO: Make custom component to handle edit/reorder/delete
+        return (
+            <TableRowHead>
+                <TableCellHead>{i18n.t('Row name')}</TableCellHead>
+                {table.columns.map((col, idx) => (
+                    <TableCellHead data-idx={idx} key={idx}>
+                        {col.name}
+                    </TableCellHead>
+                ))}
+            </TableRowHead>
+        )
+    }
+
+    function mapCellsToJsx(cells) {
+        return cells.map((cell, idx) => (
+            // TODO: Add props
+            <DataEntryCell data-idx={idx} key={idx} dispatch={dispatch} />
+        ))
+    }
+
+    function tableRows() {
+        return table.rows.map((row, idx) => (
+            // TODO: Make custom header to handle edit/reorder/deleteuops
+            <TableRow data-idx={idx} key={idx}>
+                <TableCell>{row.name}</TableCell>
+                {mapCellsToJsx(row.cells)}
+            </TableRow>
+        ))
+    }
     return (
         <>
+            {/* TODO: Make `Add Row/Column` components */}
             <ButtonStrip>
                 <Button primary>{i18n.t('+ Row')}</Button>
                 <Button primary>{i18n.t('+ Column')}</Button>
             </ButtonStrip>
             <Table>
-                <TableHead>
-                    <TableRowHead>
-                        <TableCellHead>{i18n.t('Row name')}</TableCellHead>
-                        <TableCellHead>{i18n.t('Row name')}</TableCellHead>
-                    </TableRowHead>
-                </TableHead>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>Cell 1</TableCell>
-                        <DataEntryCell />
-                    </TableRow>
-                </TableBody>
+                <TableHead>{tableColumns()}</TableHead>
+                <TableBody>{tableRows()}</TableBody>
             </Table>
         </>
     )
