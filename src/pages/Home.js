995ed70156aadda611ee2c74ce516046dbc82016
Kai Vandivier <kpvandivier@gmail.com>
2020-09-15 22:06:04 -0700

    feat: add welcome message

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,12 @@
 import React from 'react'
 import i18n from '../locales'
 
-export const Home = () => <h1>{i18n.t('Hello, world!')}</h1>
+export const Home = () => (
+    <div>
+        <h1>{i18n.t('Hello, world!')}</h1>
+        <p>
+            Hello! This is a work in progress. Take a look at the &ldquo;Create
+            custom table template&rdquo; page for recent updates.
+        </p>
+    </div>
+)
