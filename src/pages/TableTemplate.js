8068f48165074b943aa00ddd1dd31ade3d235386
Kai Vandivier <kpvandivier@gmail.com>
2020-09-26 17:16:06 -0700

    feat: add template list view

--- src/pages/TableTemplate.js
+++ src/pages/TableTemplate.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import i18n from '../locales'
-import { TemplatingTable } from './table-template/ModifyTemplate'
+import { DataStoreProvider } from '@dhis2/app-service-datastore'
+
+import { EditTableTemplate } from './table-template/EditTableTemplate'
+import { SavedTableTemplates } from './table-template/SavedTableTemplates/SavedTableTemplates'
+
+// TODO: Rename 'table templateS'
 
 // TODO: Open up a saved template or create a new one
 // Query saved templates to display in a list
 // Button to create new template (fires mutation to save new object); opens up "Modify template window"
 
-export const TableTemplate = () => (
-    <>
-        <h1>{i18n.t('Create Custom Table Template')}</h1>
-        <TemplatingTable />
-    </>
-)
+// How to query existing templates:
+// Use the data store
+// Get all items in a namespace
+// Get all the tables from there (not reports)
+// For each entry found, render a table with an item for each report - option to edit, rename, delete
+
+export const TableTemplate = () => {
+    const [editing, setEditing] = useState(false)
+
+    return (
+        <DataStoreProvider namespace="tableTemplates">
+            <h1>{i18n.t('Create Custom Table Template')}</h1>
+            {!editing ? (
+                <SavedTableTemplates setEditing={setEditing} />
+            ) : (
+                <EditTableTemplate />
+            )}
+        </DataStoreProvider>
+    )
+}
