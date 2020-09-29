827182bc8fa5f658c9b3751f2b9d0696a287db86
Kai Vandivier <kpvandivier@gmail.com>
2020-09-29 16:55:16 -0700

    feat: use table interface

--- src/pages/table-template/SavedTableTemplates/SavedTableTemplates.js
+++ src/pages/table-template/SavedTableTemplates/SavedTableTemplates.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import i18n from '../../../locales'
 import { useSavedObjectList } from '@dhis2/app-service-datastore'
+import {
+    Button,
+    ButtonStrip,
+    Table,
+    TableRow,
+    TableRowHead,
+    TableCell,
+    TableCellHead,
+} from '@dhis2/ui'
+
 import { CreateNewTableTemplate } from './CreateNewTableTemplate'
 
 import testTable from '../../../modules/testTable'
@@ -11,7 +22,7 @@ import testTable from '../../../modules/testTable'
 // - Make a default table to create
 // - Make functions to edit, rename, and delete each table template
 
-export function SavedTableTemplates() {
+export function SavedTableTemplates({ setEditingId }) {
     const [savedTableTemplates, tableTemplateActions] = useSavedObjectList({
         global: true,
     })
@@ -30,19 +41,45 @@ export function SavedTableTemplates() {
 
     function createNew(name) {
         tableTemplateActions.add({ ...testTable, name })
+        // TODO: Open 'view & modify' window;
+        // `setEditingId(id)`
+    }
+
+    function mapTemplatesToRows() {
+        return savedTableTemplates.map(template => (
+            <TableRow key={template.id}>
+                <TableCell>{template.name}</TableCell>
+                {/* TODO: Template actions */}
+                <TableCell>
+                    <ButtonStrip>
+                        <Button onClick={() => setEditingId(template.id)}>
+                            {i18n.t('View & Edit')}
+                        </Button>
+                        <Button destructive>{i18n.t('Delete (todo)')}</Button>
+                    </ButtonStrip>
+                </TableCell>
+            </TableRow>
+        ))
     }
 
     return (
         <>
+            <h1>Table Templates</h1>
             <button onClick={createDummyTemplate}>
                 {i18n.t('Make dummy template')}
             </button>
             <CreateNewTableTemplate createNew={createNew} />
-            <ul>
-                {savedTableTemplates.map(template => (
-                    <li key={template.id}>{template.name}</li>
-                ))}
-            </ul>
+            <Table>
+                <TableRowHead>
+                    <TableCellHead>{i18n.t('Name')}</TableCellHead>
+                    <TableCellHead>{i18n.t('Actions')}</TableCellHead>
+                </TableRowHead>
+                {mapTemplatesToRows()}
+            </Table>
         </>
     )
+}
+
+SavedTableTemplates.propTypes = {
+    setEditingId: PropTypes.func.isRequired,
 }
