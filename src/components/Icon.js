8875c9aba2178a7e6118e8721417700761183872
Kai Vandivier <kpvandivier@gmail.com>
2020-10-01 20:49:00 -0700

    feat: make color flexible

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -2,17 +2,21 @@ import React from 'react'
 import { colors } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 
-export function Icon({ name, dense }) {
+export function Icon({ name, dense, color = colors.grey800 }) {
     return (
         <span
             className="material-icons"
-            style={{ color: colors.grey800, fontSize: dense ? '16px' : '24px' }}
+            style={{ color, fontSize: dense ? '16px' : '24px' }}
         >
             {name}
         </span>
     )
 }
 
-Icon.propTypes = { name: PropTypes.string.isRequired, dense: PropTypes.bool }
+Icon.propTypes = {
+    name: PropTypes.string.isRequired,
+    color: PropTypes.string,
+    dense: PropTypes.bool,
+}
 
 export default Icon
