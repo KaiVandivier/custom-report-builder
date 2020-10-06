4d2575ed10af0a069330fe72890598cb43c79825
Kai Vandivier <kpvandivier@gmail.com>
2020-10-06 16:55:45 -0700

    fix: remove tests

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,6 @@
 import React from 'react'
 import i18n from '../locales'
 
-import { DataStoreTest } from './home/DataStoreTest'
-import { DataStoreProvider } from '@dhis2/app-service-datastore'
-import FlyoutMenuTest from './home/FlyoutMenuTest'
-import ColumnActions from './tables/edit-table-template/ColumnActions'
-
 export const Home = () => (
     <div>
         <h1>{i18n.t('Hello, world!')}</h1>
@@ -13,22 +8,5 @@ export const Home = () => (
             Hello! This is a work in progress. Take a look at the &ldquo;Create
             custom table template&rdquo; page for recent updates.
         </p>
-        <ColumnActions />
-        <p>
-            <span className="material-icons">arrow_drop_down</span>
-            <span className="material-icons">edit</span>
-            <span className="material-icons">delete</span>
-            <span className="material-icons">arrow_drop_up</span>
-        </p>
-        <p>
-            <span className="material-icons">arrow_left</span>
-            <span className="material-icons">edit</span>
-            <span className="material-icons">delete</span>
-            <span className="material-icons">arrow_right</span>
-        </p>
-        <FlyoutMenuTest />
-        <DataStoreProvider namespace="customReports">
-            <DataStoreTest />
-        </DataStoreProvider>
     </div>
 )
