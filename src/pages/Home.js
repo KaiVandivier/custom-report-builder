0f20de1c5e4eb833a3679e862d19f11385bdb5d4
Kai Vandivier <kpvandivier@gmail.com>
2020-10-07 21:51:24 -0700

    test: more messin' around

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import i18n from '../locales'
 
 // import IconTest from './home/IconTest'
 // import PeriodSelectTest from './home/PeriodSelectTest'
-import OrganisationUnitPicker from '../components/OrganisationUnitPicker'
+// import OrganisationUnitPicker from '../components/OrganisationUnitPicker'
+import ReportParameters from '../components/ReportParameters'
 
 export const Home = () => {
-    const [selectedOrgUnits, setSelectedOrgUnits] = useState([])
-    console.log('selected OrgUnits', selectedOrgUnits)
+    // const [selectedOrgUnits, setSelectedOrgUnits] = useState([])
+    // console.log('selected OrgUnits', selectedOrgUnits)
 
     return (
         <div>
@@ -17,11 +18,7 @@ export const Home = () => {
                 &ldquo;Create custom table template&rdquo; page for recent
                 updates.
             </p>
-            <OrganisationUnitPicker
-                selectedOrgUnits={selectedOrgUnits}
-                setSelectedOrgUnits={setSelectedOrgUnits}
-            />
-            {/* <PeriodSelectTest /> */}
+            <ReportParameters onGenerate={console.log} />
         </div>
     )
 }
