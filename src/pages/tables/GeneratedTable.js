7eae546420e3d5600a9714c30a4a287e12dfcbe0
Kai Vandivier <kpvandivier@gmail.com>
2020-10-08 02:41:58 -0700

    feat: incorporate reportParams dialog

--- src/pages/tables/GeneratedTable.js
+++ src/pages/tables/GeneratedTable.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Button, Card } from '@dhis2/ui'
 import i18n from '../../locales'
 import { useReactToPrint } from 'react-to-print'
@@ -6,15 +6,34 @@ import { useReactToPrint } from 'react-to-print'
 import BackButton from '../../components/BackButton'
 import Icon from '../../components/Icon'
 import classes from './styles/GeneratedTable.module.css'
-import TableWithData from './generated-table/TableWithData'
+import { ReportParameters, TableWithData } from './generated-table'
 
-// TODO: Make two components to avoid double dataStore queries (unless they're cached)
 export function GeneratedTable() {
+    const [reportParams, setReportParams] = useState({
+        selectedOrgUnits: [],
+        selectedPeriods: [],
+    })
+    const [reportParamsDialogOpen, setReportParamsDialogOpen] = useState(true)
+    const toggleReportParamsDialog = () =>
+        setReportParamsDialogOpen(state => !state)
+
     const printRef = useRef()
     const handlePrint = useReactToPrint({ content: () => printRef.current })
 
     return (
         <div id="generated-table">
+            <ReportParameters
+                open={reportParamsDialogOpen}
+                toggleModal={toggleReportParamsDialog}
+                onGenerate={setReportParams}
+            />
+            <Button
+                large
+                icon={<Icon name="play_arrow" />}
+                onClick={toggleReportParamsDialog}
+            >
+                {i18n.t('Choose Parameters')}
+            </Button>
             <div className={classes.topButtons}>
                 <BackButton to="/tables" />
                 <Button
@@ -27,7 +46,7 @@ export function GeneratedTable() {
             </div>
             <Card>
                 <div ref={printRef} className={classes.print}>
-                    <TableWithData />
+                    <TableWithData {...reportParams} />
                 </div>
             </Card>
         </div>
