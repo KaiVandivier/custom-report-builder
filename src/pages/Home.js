9717688a92ca67123cdeb7513bc0bd80f1209818
Kai Vandivier <kpvandivier@gmail.com>
2020-10-07 17:11:32 -0700

    feat: make fully-fledged org unit picker

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import i18n from '../locales'
 
 // import IconTest from './home/IconTest'
+// import PeriodSelectTest from './home/PeriodSelectTest'
 import OrgUnitTreeTest from './home/OrgUnitTreeTest'
-import PeriodSelectTest from './home/PeriodSelectTest'
 
-export const Home = () => (
-    <div>
-        <h1>{i18n.t('Hello, world!')}</h1>
-        <p>
-            Hello! This is a work in progress. Take a look at the &ldquo;Create
-            custom table template&rdquo; page for recent updates.
-        </p>
-        <OrgUnitTreeTest />
-        <PeriodSelectTest />
-    </div>
-)
+export const Home = () => {
+    const [selectedOrgUnits, setSelectedOrgUnits] = useState([])
+    console.log('selected OrgUnits', selectedOrgUnits)
+
+    return (
+        <div>
+            <h1>{i18n.t('Hello, world!')}</h1>
+            <p>
+                Hello! This is a work in progress. Take a look at the
+                &ldquo;Create custom table template&rdquo; page for recent
+                updates.
+            </p>
+            <OrgUnitTreeTest
+                selectedOrgUnits={selectedOrgUnits}
+                setSelectedOrgUnits={setSelectedOrgUnits}
+            />
+            {/* <PeriodSelectTest /> */}
+        </div>
+    )
+}
--- src/pages/home/OrgUnitTreeTest.js
+++ src/pages/home/OrgUnitTreeTest.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import { OrganisationUnitTree } from '@dhis2/ui'
+import PropTypes from 'prop-types'
+import { OrganisationUnitTree, CircularLoader } from '@dhis2/ui'
 import { useDataQuery } from '@dhis2/app-runtime'
 
 const ORG_UNIT_QUERY = {
@@ -11,23 +12,43 @@ const ORG_UNIT_QUERY = {
     },
 }
 
-export default function OrgUnitTreeTest() {
-    const [selected, setSelected] = useState([])
+export default function OrganisationUnitPicker({
+    selectedOrgUnits,
+    setSelectedOrgUnits,
+}) {
+    const [selectedPaths, setSelectedPaths] = useState([])
     const { data, loading, error } = useDataQuery(ORG_UNIT_QUERY)
-    if (loading) return <p>Loading...</p>
+
+    if (loading) return <CircularLoader small />
     if (error) return <p>Oops! Something went wrong.</p>
+
     const roots = data.result.organisationUnits.map(({ id }) => id)
 
-    const onChange = ({ id, path, checked }) => {
-        console.log('onChange', { path, id, checked })
-        const pathIndex = selected.indexOf(path)
+    const updateSelectedOrgUnits = ({ id, displayName, checked }) => {
+        const itemIndex = selectedOrgUnits.findIndex(item => id === item.id)
 
         if (checked) {
-            setSelected([...selected, path])
+            setSelectedOrgUnits([
+                ...selectedOrgUnits,
+                { id, name: displayName },
+            ])
         } else {
-            setSelected([
-                ...selected.slice(0, pathIndex),
-                ...selected.slice(pathIndex + 1),
+            setSelectedOrgUnits([
+                ...selectedOrgUnits.slice(0, itemIndex),
+                ...selectedOrgUnits.slice(itemIndex + 1),
+            ])
+        }
+    }
+
+    const onChange = ({ path, checked }) => {
+        const pathIndex = selectedPaths.indexOf(path)
+
+        if (checked) {
+            setSelectedPaths([...selectedPaths, path])
+        } else {
+            setSelectedPaths([
+                ...selectedPaths.slice(0, pathIndex),
+                ...selectedPaths.slice(pathIndex + 1),
             ])
         }
     }
@@ -35,8 +56,21 @@ export default function OrgUnitTreeTest() {
     return (
         <OrganisationUnitTree
             roots={roots}
-            onChange={onChange}
-            selected={selected}
+            onChange={args => {
+                onChange(args)
+                updateSelectedOrgUnits(args)
+            }}
+            selected={selectedPaths}
         />
     )
+}
+
+OrganisationUnitPicker.propTypes = {
+    selectedOrgUnits: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.string,
+            name: PropTypes.string,
+        })
+    ).isRequired,
+    setSelectedOrgUnits: PropTypes.func.isRequired,
 }
