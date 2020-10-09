af6a71693b432f78c41f229404403f8527cd0a1c
Kai Vandivier <kpvandivier@gmail.com>
2020-10-09 16:10:49 -0700

    refactor: change to redirect

--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,24 +1,6 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import i18n from '../locales'
+import { Redirect } from 'react-router-dom'
 
-export function NoMatch({ location }) {
-    console.log(location)
-    return (
-        <div>
-            <h3>
-                {i18n.t("No page found for URL '{{- url}}'", {
-                    url: location.pathname,
-                })}
-            </h3>
-        </div>
-    )
-}
-
-NoMatch.propTypes = {
-    location: PropTypes.shape({
-        pathname: PropTypes.string,
-    }).isRequired,
-}
+export const NoMatch = () => <Redirect to="/" />
 
 export default NoMatch
