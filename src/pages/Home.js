dcaaa2235e77cc99aadad134bb578e53fc818aeb
Kai Vandivier <kpvandivier@gmail.com>
2020-10-08 20:46:22 -0700

    feat: update homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,21 @@
 import React from 'react'
 import i18n from '../locales'
+import { Link } from 'react-router-dom'
 
 // import IconTest from './home/IconTest'
 // import PeriodSelectTest from './home/PeriodSelectTest'
 // import OrganisationUnitPicker from '../components/OrganisationUnitPicker'
-import ReportParameters from './tables/generated-table/ReportParameters'
+// import ReportParameters from './tables/generated-table/ReportParameters'
 
 export const Home = () => {
-    // const [selectedOrgUnits, setSelectedOrgUnits] = useState([])
-    // console.log('selected OrgUnits', selectedOrgUnits)
-
     return (
         <div>
             <h1>{i18n.t('Hello, world!')}</h1>
             <p>
-                Hello! This is a work in progress. Take a look at the
-                &ldquo;Create custom table template&rdquo; page for recent
-                updates.
+                Hello! This is a work in progress. Take a look at the{' '}
+                <Link to="/tables">&ldquo;Custom tables&rdquo; page</Link> for
+                recent updates.
             </p>
-            <ReportParameters onGenerate={console.log} />
         </div>
     )
 }
