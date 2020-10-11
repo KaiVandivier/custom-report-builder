3998542927850b55f973458626cfd274b5a97cdf
Kai Vandivier <kpvandivier@gmail.com>
2020-10-11 14:07:19 -0700

    fix: comment

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import classes from './App.module.css'
 import { Navigation } from './navigation'
 import { Home, Tables, Reports, NoMatch } from './pages'
 
+/*
+ * HashRouter solves the problem of route issues on DHIS instances deployed
+ * at addresses other than a base domain, e.g. `play.dhis2.org/2.34.1/` or
+ * `academy.demos.dhis2.org/app-dev-academy`.
+ * BrowserRouter with `basename="/api/apps/app-name" can work for apps
+ * deployed at domain level.
+ */
 const MyApp = () => {
-    // HashRouter solves the problem of route issues on DHIS instances deployed
-    // at addresses other than a base domain, e.g. `play.dhis2.org/2.34.1/` or
-    // `academy.demos.dhis2.org/app-dev-academy`.
-    // BrowserRouter with `basename="/api/apps/app-name" can work for apps
-    // deployed at domain level.
     return (
         <HashRouter>
             <div className={classes.container}>
