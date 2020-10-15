0c0272214c219941605319676cd7795c988fe8a4
Kai Vandivier <kpvandivier@gmail.com>
2020-10-15 02:37:35 -0700

    fix: spacing somewhat

--- src/components/styles/IconTooltipButton.style.js
+++ src/components/styles/IconTooltipButton.style.js
@@ -2,6 +2,8 @@ import css from 'styled-jsx/css'
 
 export default css`
     button {
+        display: flex;
+        align-items: center;
         border: none;
         background: none;
         cursor: pointer;
