a0f0828e7ea00b9043e26da1f3e12f08193ca05c
Kai Vandivier <kpvandivier@gmail.com>
2020-10-14 17:27:15 -0700

    feat: content types selector

---
+++ src/pages/tables/edit-table-template/ContentTypeSelector.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { SingleSelectField, SingleSelectOption } from '@dhis2/ui'
+import i18n from '../../../locales'
+import contentTypes from '../../../modules/contentTypes'
+
+export function ContentTypeSelector({ currentContentType, onChange }) {
+    return (
+        <SingleSelectField
+            label={i18n.t('Content type')}
+            onChange={onChange}
+            selected={currentContentType}
+        >
+            {Object.values(contentTypes).map(({ id, getName }) => (
+                <SingleSelectOption value={id} label={getName()} key={id} />
+            ))}
+        </SingleSelectField>
+    )
+}
+
+ContentTypeSelector.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    currentContentType: PropTypes.string,
+}
+
+export default ContentTypeSelector
