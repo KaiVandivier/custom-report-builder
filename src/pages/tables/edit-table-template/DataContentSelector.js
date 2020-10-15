e4f842c459701148aaeec786ee438d70fad774f8
Kai Vandivier <kpvandivier@gmail.com>
2020-10-15 02:59:04 -0700

    feat: layout & tooltip icon buttons

--- src/pages/tables/edit-table-template/DataContentSelector.js
+++ src/pages/tables/edit-table-template/DataContentSelector.js
@@ -8,6 +8,7 @@ import { dataTypes } from '../../../modules/dataTypes'
 import DataEngine from '../../../components/DataEngine'
 import DataSelectorModal from './DataSelector/DataSelectorModal'
 import styles from './styles/DataContentSelector.style'
+import IconTooltipButton from '../../../components/IconTooltipButton'
 
 export function DataContentSelector({ cell, dispatch, rowIdx, cellIdx }) {
     const [modalOpen, setModalOpen] = useState(false)
@@ -35,16 +36,63 @@ export function DataContentSelector({ cell, dispatch, rowIdx, cellIdx }) {
             <Divider />
             {data.item ? (
                 <>
-                    <div className="header">{i18n.t('Name')}</div>
-                    <p>{data.item.name}</p>
-                    <div className="header">{i18n.t('Data Type')}</div>
-                    <p>
-                        {dataTypes[data.dataType].getName().replace(/s$/, '')}
-                    </p>
-                    <div className="header">{i18n.t('Org. Unit(s)')}</div>
-                    <p>{i18n.t('Same as table')}</p>
-                    <div className="header">{i18n.t('Period(s)')}</div>
-                    <p>{i18n.t('Same as table')}</p>
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <div>
+                            <div className="header">{i18n.t('Name')}</div>
+                            <p>{data.item.name}</p>
+                            <div className="header">{i18n.t('Data Type')}</div>
+                            <p>
+                                {dataTypes[data.dataType]
+                                    .getName()
+                                    .replace(/s$/, '')}
+                            </p>
+                        </div>
+                        {/* <IconTooltipButton
+                            icon="edit"
+                            tooltip={i18n.t('Select data')}
+                        /> */}
+                    </div>
+
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <div>
+                            <div className="header">
+                                {i18n.t('Org. Unit(s)')}
+                            </div>
+                            <p>{i18n.t('Same as table')}</p>
+                        </div>
+                        <IconTooltipButton
+                            icon="edit"
+                            tooltip={i18n.t('Select org. unit(s)')}
+                        />
+                    </div>
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <div>
+                            <div className="header">{i18n.t('Period(s)')}</div>
+                            <p>{i18n.t('Same as table')}</p>
+                        </div>
+                        <IconTooltipButton
+                            icon="edit"
+                            tooltip={i18n.t('Select period(s)')}
+                        />
+                    </div>
                 </>
             ) : (
                 <p>{i18n.t('No data selected')}</p>
