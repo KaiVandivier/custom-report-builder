b4e55b0d0fc9261723ed7883cff2cf4476e67821
Kai Vandivier <kpvandivier@gmail.com>
2020-10-16 19:51:16 -0700

    style: global styles for headers and text color

---
+++ src/styles/global.style.js
@@ -0,0 +1,12 @@
+import css from 'styled-jsx/css'
+import { colors } from '@dhis2/ui'
+
+export default css.global`
+    h1 {
+        font-weight: 300;
+    }
+
+    body {
+        color: ${colors.grey900};
+    }
+`
