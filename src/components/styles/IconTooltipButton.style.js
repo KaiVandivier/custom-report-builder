315f3e2e4ecfdc2e1c07448566698fbc9c887ffe
Kai Vandivier <kpvandivier@gmail.com>
2020-10-16 18:53:45 -0700

    style: change color to dark grey

--- src/components/styles/IconTooltipButton.style.js
+++ src/components/styles/IconTooltipButton.style.js
@@ -1,9 +1,11 @@
 import css from 'styled-jsx/css'
+import { colors } from '@dhis2/ui'
 
 export default css`
     button {
         border: none;
         background: none;
         cursor: pointer;
+        color: ${colors.grey800};
     }
 `
