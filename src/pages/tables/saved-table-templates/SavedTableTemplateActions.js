f19e9a51db76ab208987e2497c48057b610f63d3
Kai Vandivier <kpvandivier@gmail.com>
2020-10-16 18:59:27 -0700

    refactor: use PopoverMenu component

--- src/pages/tables/saved-table-templates/SavedTableTemplateActions.js
+++ src/pages/tables/saved-table-templates/SavedTableTemplateActions.js
@@ -1,62 +1,42 @@
-import React, { useState, useRef } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { Button, FlyoutMenu, MenuItem, Popover } from '@dhis2/ui'
+import { MenuItem } from '@dhis2/ui'
 import i18n from '../../../locales'
 
 import Icon from '../../../components/Icon'
 import DeleteTableTemplate from './DeleteTableTemplate'
-
-// TODO: Execute actions on click (and close the menu)
+import PopoverMenu from '../../../components/PopoverMenu'
 
 export function SavedTableTemplateActions({ onGenerate, onEdit, onDelete }) {
-    const [popoverIsOpen, setPopoverIsOpen] = useState(false)
-    const buttonRef = useRef()
-
-    const togglePopover = () => {
-        setPopoverIsOpen(prevState => !prevState)
-    }
-
     return (
-        <div>
-            <span ref={buttonRef}>
-                <Button small onClick={togglePopover}>
-                    <span className="material-icons">more_horiz</span>
-                </Button>
-            </span>
-            {popoverIsOpen && (
-                <Popover
-                    arrow={false}
-                    reference={buttonRef}
-                    placement="bottom-start"
-                    onClickOutside={togglePopover}
-                >
-                    <FlyoutMenu>
-                        <MenuItem
-                            icon={<Icon name="play_arrow" />}
-                            label={i18n.t('Generate Table')}
-                            onClick={() => {
-                                onGenerate()
-                                togglePopover()
-                            }}
-                        />
-                        <MenuItem
-                            icon={<Icon name="edit" />}
-                            label={i18n.t('View & Edit')}
-                            onClick={() => {
-                                onEdit()
-                                togglePopover()
-                            }}
-                        />
-                        <DeleteTableTemplate
-                            onDeleteConfirmation={() => {
-                                onDelete()
-                                togglePopover()
-                            }}
-                        />
-                    </FlyoutMenu>
-                </Popover>
+        <PopoverMenu tooltip={i18n.t('Table actions')}>
+            {togglePopover => (
+                <>
+                    <MenuItem
+                        icon={<Icon name="play_arrow" />}
+                        label={i18n.t('Generate Table')}
+                        onClick={() => {
+                            onGenerate()
+                            togglePopover()
+                        }}
+                    />
+                    <MenuItem
+                        icon={<Icon name="edit" />}
+                        label={i18n.t('View & Edit')}
+                        onClick={() => {
+                            onEdit()
+                            togglePopover()
+                        }}
+                    />
+                    <DeleteTableTemplate
+                        onDeleteConfirmation={() => {
+                            onDelete()
+                            togglePopover()
+                        }}
+                    />
+                </>
             )}
-        </div>
+        </PopoverMenu>
     )
 }
 
