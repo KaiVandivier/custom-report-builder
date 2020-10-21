05e15228bcd2d10d415389a4a0ebd0273f98ed49
Kai Vandivier <kpvandivier@gmail.com>
2020-10-21 15:14:31 -0700

    feat: assign dimensions to column

--- src/pages/tables/edit-table-template/ColumnActions.js
+++ src/pages/tables/edit-table-template/ColumnActions.js
@@ -5,6 +5,7 @@ import {
     DELETE_COLUMN,
     REORDER_COLUMN,
     UPDATE_COLUMN,
+    UPDATE_COLUMN_DIMENSIONS,
 } from '../../../reducers/tableReducer'
 import i18n from '../../../locales'
 
@@ -12,10 +13,24 @@ import Icon from '../../../components/Icon'
 import PopoverButton from '../../../components/PopoverButton'
 import ConfirmModal from '../../../components/ConfirmModal'
 import InputModal from '../../../components/InputModal'
+import DataEngine from '../../../components/DataEngine'
+import {
+    DataSelectorDialog,
+    OrgUnitSelectorDialog,
+    PeriodSelectorDialog,
+} from './EditTableCell'
 
 export function ColumnActions({ dispatch, col, idx, maxIdx }) {
     const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false)
     const [editModalIsOpen, setEditModalIsOpen] = useState(false)
+
+    const [dataDialogOpen, setDataDialogOpen] = useState(false)
+    const [orgUnitDialogOpen, setOrgUnitDialogOpen] = useState(false)
+    const [periodDialogOpen, setPeriodDialogOpen] = useState(false)
+
+    const toggleDataDialog = () => setDataDialogOpen(state => !state)
+    const toggleOrgUnitDialog = () => setOrgUnitDialogOpen(state => !state)
+    const togglePeriodDialog = () => setPeriodDialogOpen(state => !state)
 
     function onMoveLeft(togglePopover) {
         if (idx <= 0) return
@@ -51,83 +66,158 @@ export function ColumnActions({ dispatch, col, idx, maxIdx }) {
         togglePopover()
     }
 
+    function onDataDialogSave(data) {
+        dispatch({
+            type: UPDATE_COLUMN_DIMENSIONS,
+            payload: {
+                idx,
+                dimensions: { ...data },
+            },
+        })
+    }
+
+    function onOrgUnitDialogSave(orgUnits) {
+        dispatch({
+            type: UPDATE_COLUMN_DIMENSIONS,
+            payload: {
+                idx,
+                dimensions: { orgUnits },
+            },
+        })
+    }
+
+    function onPeriodDialogSave(periods) {
+        dispatch({
+            type: UPDATE_COLUMN_DIMENSIONS,
+            payload: {
+                idx,
+                dimensions: { periods },
+            },
+        })
+    }
+
     return (
-        <PopoverButton tooltip={i18n.t('Column actions')}>
-            {togglePopover => (
-                <FlyoutMenu>
-                    <MenuItem
-                        dense
-                        icon={<Icon name="assignment" dense />}
-                        label={i18n.t('Assign dimensions to column')}
-                        onClick={args => {
-                            // TODO
-                            console.log(args)
-                            console.log('Assigning')
-                        }}
-                    >
-                        <MenuItem dense label={i18n.t('Data Item')} />
+        <>
+            <PopoverButton tooltip={i18n.t('Column actions')}>
+                {togglePopover => (
+                    <FlyoutMenu>
                         <MenuItem
                             dense
-                            label={i18n.t('Organisation Unit(s)')}
+                            icon={<Icon name="assignment" dense />}
+                            label={i18n.t('Assign dimensions to column')}
+                        >
+                            <MenuItem
+                                dense
+                                label={i18n.t('Data Item')}
+                                onClick={() => {
+                                    toggleDataDialog()
+                                    togglePopover()
+                                }}
+                            />
+                            <MenuItem
+                                dense
+                                label={i18n.t('Organisation Unit(s)')}
+                                onClick={() => {
+                                    toggleOrgUnitDialog()
+                                    togglePopover()
+                                }}
+                            />
+                            <MenuItem
+                                dense
+                                label={i18n.t('Period(s)')}
+                                onClick={() => {
+                                    togglePeriodDialog()
+                                    togglePopover()
+                                }}
+                            />
+                        </MenuItem>
+                        <MenuItem
+                            dense
+                            disabled={idx <= 0}
+                            icon={<Icon name="arrow_left" dense />}
+                            label={i18n.t('Move column left')}
+                            onClick={() => onMoveLeft(togglePopover)}
                         />
-                        <MenuItem dense label={i18n.t('Period(s)')} />
-                    </MenuItem>
-                    <MenuItem
-                        dense
-                        disabled={idx <= 0}
-                        icon={<Icon name="arrow_left" dense />}
-                        label={i18n.t('Move column left')}
-                        onClick={() => onMoveLeft(togglePopover)}
-                    />
-                    <MenuItem
-                        dense
-                        disabled={idx >= maxIdx}
-                        icon={<Icon name="arrow_right" dense />}
-                        label={i18n.t('Move column right')}
-                        onClick={() => onMoveRight(togglePopover)}
-                    />
-                    <MenuItem
-                        dense
-                        icon={<Icon name="edit" dense />}
-                        label={i18n.t('Edit')}
-                        onClick={() => setEditModalIsOpen(true)}
-                    />
-                    <MenuItem
-                        dense
-                        icon={<Icon name="delete" dense />}
-                        label={i18n.t('Delete')}
-                        onClick={() => setDeleteModalIsOpen(true)}
-                    />
-                    {deleteModalIsOpen && (
-                        <ConfirmModal
-                            confirmText={i18n.t('Delete')}
-                            text={i18n.t('Do you want to delete this column?')}
-                            title={i18n.t('Confirm deletion')}
-                            onCancel={() => setDeleteModalIsOpen(false)}
-                            onConfirm={() => {
-                                onDelete(togglePopover)
-                                setDeleteModalIsOpen(false)
-                            }}
-                            destructive={true}
+                        <MenuItem
+                            dense
+                            disabled={idx >= maxIdx}
+                            icon={<Icon name="arrow_right" dense />}
+                            label={i18n.t('Move column right')}
+                            onClick={() => onMoveRight(togglePopover)}
+                        />
+                        <MenuItem
+                            dense
+                            icon={<Icon name="edit" dense />}
+                            label={i18n.t('Edit')}
+                            onClick={() => setEditModalIsOpen(true)}
+                        />
+                        <MenuItem
+                            dense
+                            icon={<Icon name="delete" dense />}
+                            label={i18n.t('Delete')}
+                            onClick={() => setDeleteModalIsOpen(true)}
+                        />
+                        {deleteModalIsOpen && (
+                            <ConfirmModal
+                                confirmText={i18n.t('Delete')}
+                                text={i18n.t(
+                                    'Do you want to delete this column?'
+                                )}
+                                title={i18n.t('Confirm deletion')}
+                                onCancel={() => setDeleteModalIsOpen(false)}
+                                onConfirm={() => {
+                                    onDelete(togglePopover)
+                                    setDeleteModalIsOpen(false)
+                                }}
+                                destructive={true}
+                            />
+                        )}
+                        {editModalIsOpen && (
+                            <InputModal
+                                title={i18n.t('Edit column')}
+                                inputLabel={i18n.t('Column name')}
+                                inputPlaceholder={i18n.t('Enter column name')}
+                                confirmText={i18n.t('Save')}
+                                onCancel={() => setEditModalIsOpen(false)}
+                                onConfirm={inputText => {
+                                    onEdit(togglePopover, inputText)
+                                    setEditModalIsOpen(false)
+                                }}
+                                initialValue={col.name}
+                            />
+                        )}
+                    </FlyoutMenu>
+                )}
+            </PopoverButton>
+            {dataDialogOpen && (
+                <DataEngine>
+                    {engine => (
+                        <DataSelectorDialog
+                            engine={engine}
+                            onClose={toggleDataDialog}
+                            onSave={onDataDialogSave}
+                            initialValues={
+                                col.dimensions?.item
+                                    ? { ...col.dimensions }
+                                    : {}
+                            }
                         />
                     )}
-                    {editModalIsOpen && (
-                        <InputModal
-                            title={i18n.t('Edit column')}
-                            inputLabel={i18n.t('Column name')}
-                            inputPlaceholder={i18n.t('Enter column name')}
-                            confirmText={i18n.t('Save')}
-                            onCancel={() => setEditModalIsOpen(false)}
-                            onConfirm={inputText => {
-                                onEdit(togglePopover, inputText)
-                                setEditModalIsOpen(false)
-                            }}
-                            initialValue={col.name}
-                        />
-                    )}
-                </FlyoutMenu>
+                </DataEngine>
             )}
-        </PopoverButton>
+            <OrgUnitSelectorDialog
+                open={orgUnitDialogOpen}
+                currentlySelected={col.dimensions?.orgUnits}
+                toggleModal={toggleOrgUnitDialog}
+                onSave={onOrgUnitDialogSave}
+            />
+            <PeriodSelectorDialog
+                open={periodDialogOpen}
+                currentlySelected={col.dimensions?.periods}
+                toggleModal={togglePeriodDialog}
+                onSave={onPeriodDialogSave}
+            />
+        </>
     )
 }
 
@@ -137,6 +227,27 @@ ColumnActions.propTypes = {
     maxIdx: PropTypes.number.isRequired,
     col: PropTypes.shape({
         name: PropTypes.string.isRequired,
+        dimensions: PropTypes.shape({
+            dataType: PropTypes.string,
+            group: PropTypes.string,
+            groupDetail: PropTypes.string,
+            item: PropTypes.shape({
+                id: PropTypes.string,
+                name: PropTypes.string,
+            }),
+            orgUnits: PropTypes.arrayOf(
+                PropTypes.shape({
+                    id: PropTypes.string,
+                    name: PropTypes.string,
+                })
+            ),
+            periods: PropTypes.arrayOf(
+                PropTypes.shape({
+                    id: PropTypes.string,
+                    name: PropTypes.string,
+                })
+            ),
+        }),
     }),
 }
 
