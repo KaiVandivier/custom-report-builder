6fdb6c9811db58ab14666c272262cd7e813c5717
Kai Vandivier <kpvandivier@gmail.com>
2020-10-22 16:24:16 -0700

    style: smaller min-width for grid items

--- src/pages/styles/Home.style.js
+++ src/pages/styles/Home.style.js
@@ -3,7 +3,7 @@ import css from 'styled-jsx/css'
 export default css`
     .grid-container {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
         gap: 1rem;
     }
 `
