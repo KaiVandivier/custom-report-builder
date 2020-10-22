457db497da2b0d5b5375dcdcc7549c7a1c6449ff
Kai Vandivier <kpvandivier@gmail.com>
2020-10-22 16:22:05 -0700

    fix: element organization

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,34 +5,32 @@ import { NavCard } from './home'
 
 export const Home = () => {
     return (
-        <div>
-            <div className="grid-container">
-                <NavCard
-                    title={i18n.t('Custom Tables')}
-                    icon="table_view"
-                    content={i18n.t('Choose contents on a cell-by-cell basis.')}
-                    action={i18n.t('View Tables')}
-                    path="/tables"
-                />
-                <NavCard
-                    title={i18n.t('Custom Reports')}
-                    icon="article"
-                    content={i18n.t(
-                        'Add text, images, and DHIS assets to a printable report.'
-                    )}
-                    action={i18n.t('View Reports')}
-                    path="/reports"
-                />
-                <NavCard
-                    title={i18n.t('Information')}
-                    icon="help"
-                    content={i18n.t(
-                        'View instructions for use of the Custom Tables and Custom Reports tools.'
-                    )}
-                    action={i18n.t('View Tables')}
-                    path="/information"
-                />
-            </div>
+        <div className="grid-container">
+            <NavCard
+                title={i18n.t('Custom Tables')}
+                icon="table_view"
+                content={i18n.t('Choose contents on a cell-by-cell basis.')}
+                action={i18n.t('View Tables')}
+                path="/tables"
+            />
+            <NavCard
+                title={i18n.t('Custom Reports')}
+                icon="article"
+                content={i18n.t(
+                    'Add text, images, and DHIS assets to a printable report.'
+                )}
+                action={i18n.t('View Reports')}
+                path="/reports"
+            />
+            <NavCard
+                title={i18n.t('Information')}
+                icon="help"
+                content={i18n.t(
+                    'View instructions for use of the Custom Tables and Custom Reports tools.'
+                )}
+                action={i18n.t('View Tables')}
+                path="/information"
+            />
             <style jsx>{styles}</style>
         </div>
     )
