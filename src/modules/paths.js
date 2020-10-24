6d27eae2b13736d05f06ede3a078e55705b66d70
Kai Vandivier <kpvandivier@gmail.com>
2020-10-23 22:31:35 -0700

    refactor: paths enum

---
+++ src/modules/paths.js
@@ -0,0 +1,11 @@
+import path from 'path'
+
+export const TABLES = '/tables'
+export const EDIT_TABLE = '/tables/edit'
+export const GENERATED_TABLE = 'tables/generated'
+
+export const INFORMATION = '/information'
+
+export const REPORTS = '/reports'
+
+export const getPath = (...args) => path.join(...args)
