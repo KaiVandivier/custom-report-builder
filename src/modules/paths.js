ceeb122368150beb4ddfc936495fecb3b8970534
Kai Vandivier <kpvandivier@gmail.com>
2020-10-23 22:32:26 -0700

    fix: help path

--- src/modules/paths.js
+++ src/modules/paths.js
@@ -4,7 +4,7 @@ export const TABLES = '/tables'
 export const EDIT_TABLE = '/tables/edit'
 export const GENERATED_TABLE = 'tables/generated'
 
-export const INFORMATION = '/information'
+export const HELP = '/help'
 
 export const REPORTS = '/reports'
 
