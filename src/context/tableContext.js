b92fcfe1384df58d527c123b6d702b1ca3dd6348
Kai Vandivier <kpvandivier@gmail.com>
2020-10-27 00:47:09 -0700

    refactor: make a context, provider, and hooks for tables

---
+++ src/context/tableContext.js
@@ -0,0 +1,55 @@
+import React, { useContext, useReducer } from 'react'
+import PropTypes from 'prop-types'
+import { useParams } from 'react-router-dom'
+import { useSavedObject } from '@dhis2/app-service-datastore'
+import tableReducer from '../reducers/tableReducer'
+
+const TableStateContext = React.createContext()
+const TableDispatchContext = React.createContext()
+const TableActionsContext = React.createContext()
+
+export function TableProvider({ children }) {
+    const params = useParams()
+    const [savedTable, savedTableActions] = useSavedObject(params.id)
+    const [table, dispatch] = useReducer(tableReducer, savedTable)
+
+    return (
+        <TableStateContext.Provider value={table}>
+            <TableDispatchContext.Provider value={dispatch}>
+                <TableActionsContext value={savedTableActions}>
+                    {children}
+                </TableActionsContext>
+            </TableDispatchContext.Provider>
+        </TableStateContext.Provider>
+    )
+}
+
+TableProvider.propTypes = { children: PropTypes.element }
+
+export function useTableState() {
+    const table = useContext(TableStateContext)
+    if (table === undefined) {
+        throw new Error('useTableState must be used within a TableProvider')
+    }
+    return table
+}
+
+export function useTableDispatch() {
+    const dispatch = useContext(TableDispatchContext)
+    if (dispatch === undefined) {
+        throw new Error('useTableDispatch must be used within a TableProvider')
+    }
+    return dispatch
+}
+
+export function useTableActions() {
+    const actions = useContext(TableActionsContext)
+    if (actions === undefined) {
+        throw new Error('useTableActions must be used within a TableProvider')
+    }
+    return actions
+}
+
+export function useTable() {
+    return [useTableState(), useTableDispatch(), useTableActions()]
+}
