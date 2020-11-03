632d1f401071dbac9a3434b918cbb841ba335d33
Kai Vandivier <kpvandivier@gmail.com>
2020-11-03 15:18:51 -0800

    feat: convert editor into dialog (wip)

--- src/pages/tables/edit-table-template/HighlightingEditor.js
+++ src/pages/tables/edit-table-template/HighlightingEditor.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { Button, ButtonStrip, InputFieldFF, Switch, colors } from '@dhis2/ui'
 import {
+    Modal,
+    ModalTitle,
+    ModalContent,
+    ModalActions,
     ReactFinalForm,
     hasValue,
     number,
@@ -21,11 +25,80 @@ const defaultIntervals = [
 
 const { Form, Field } = ReactFinalForm
 
+// function IntervalEditor({ values, intervals }) {
+//     /**
+//      * 'lowerBound' must have a value greater than the interval below and
+//      * lesser than the interval above. It must also do so dynamically as
+//      * the fields change with user input.
+//      */
+//     const validateField = (idx, arr) =>
+//         composeValidators(
+//             hasValue,
+//             number,
+//             createMinNumber(
+//                 idx === arr.length - 1
+//                     ? -Infinity
+//                     : Number(
+//                           values[`lowerBound-${idx + 1}`] ||
+//                               arr[idx + 1].lowerBound
+//                       )
+//             ),
+//             createMaxNumber(
+//                 idx === 0
+//                     ? Infinity
+//                     : Number(
+//                           values[`lowerBound-${idx - 1}`] ||
+//                               arr[idx - 1].lowerBound
+//                       )
+//             )
+//         )
+
+//     return intervals.map((interval, idx, arr) => (
+//         <div className="interval-container" key={idx}>
+//             <div
+//                 className="color-swatch"
+//                 style={{
+//                     backgroundColor: interval.color,
+//                 }}
+//             />
+//             {idx === arr.length - 1 ? (
+//                 idx === 0 ? (
+//                     <span>All values</span>
+//                 ) : (
+//                     <>
+//                         <span>
+//                             {i18n.t('Value {{-lt}} {{value}}', {
+//                                 lt: '\x3C',
+//                                 value: values[`lowerBound-${idx - 1}`],
+//                             })}
+//                         </span>
+//                     </>
+//                 )
+//             ) : (
+//                 <>
+//                     <span>Value &ge;</span>
+//                     <Field
+//                         dense
+//                         name={`lowerBound-${idx}`}
+//                         component={InputFieldFF}
+//                         validate={validateField(idx, arr)}
+//                         initialValue={String(interval.lowerBound)}
+//                     />
+//                 </>
+//             )}
+//             <style jsx>{styles}</style>
+//         </div>
+//     ))
+// }
+
 export function HighlightingEditor({ table, dispatch }) {
     // TODO: Get intervals from the table itself
     const [intervals, setIntervals] = useState(
         table.highlightingIntervals || defaultIntervals
     )
+    const [modalIsOpen, setModalIsOpen] = useState(false)
+
+    const toggleModal = () => setModalIsOpen(state => !state)
 
     function onSwitch() {
         dispatch({
@@ -36,6 +109,7 @@ export function HighlightingEditor({ table, dispatch }) {
 
     function onSubmit(values) {
         console.log(values)
+        // TODO: Fix me
         setIntervals([...Object.values(values)])
     }
 
@@ -106,47 +180,73 @@ export function HighlightingEditor({ table, dispatch }) {
     }
 
     return (
-        <div style={{ marginBottom: '1rem' }}>
+        <div className="switch-container">
             <Switch
                 checked={table.highlightingOn}
                 label={i18n.t('Highlight cells based on value')}
                 onChange={onSwitch}
             />
-            {table.highlightingOn && (
-                // TODO: Investigate 'name' and 'values' options
-                <Form onSubmit={onSubmit}>
-                    {({
-                        handleSubmit,
-                        form,
-                        submitting,
-                        pristine,
-                        values,
-                        initialValues,
-                    }) => (
-                        <form onSubmit={handleSubmit}>
-                            <div className="container">
-                                {getIntervalEditors(values)}
-                            </div>
-                            <ButtonStrip>
-                                <Button
-                                    primary
-                                    small
-                                    type="submit"
-                                    disabled={submitting}
-                                >
-                                    {i18n.t('Save')}
-                                </Button>
-                                <Button
-                                    small
-                                    onClick={() => form.reset(initialValues)}
-                                    disabled={submitting || pristine}
-                                >
-                                    {i18n.t('Reset')}
-                                </Button>
-                            </ButtonStrip>
-                        </form>
-                    )}
-                </Form>
+            <Button
+                small
+                disabled={!table.highlightingOn}
+                onClick={toggleModal}
+            >
+                {i18n.t('Configure')}
+            </Button>
+            {modalIsOpen && (
+                <Modal onClose={toggleModal}>
+                    <ModalTitle>
+                        {i18n.t('Edit Highlighting Intervals')}
+                    </ModalTitle>
+                    {/*TODO: Investigate 'name' and 'values' options */}
+                    <Form onSubmit={onSubmit}>
+                        {({
+                            handleSubmit,
+                            form,
+                            submitting,
+                            pristine,
+                            values,
+                            initialValues,
+                        }) => {
+                            console.log(values)
+                            return (
+                                <form onSubmit={handleSubmit}>
+                                    <ModalContent>
+                                        <div className="editor-container">
+                                            {getIntervalEditors(values)}
+                                        </div>
+                                    </ModalContent>
+                                    <ModalActions>
+                                        <ButtonStrip end>
+                                            <Button onClick={toggleModal}>
+                                                {i18n.t('Cancel')}
+                                            </Button>
+                                            <Button
+                                                onClick={() =>
+                                                    form.reset(initialValues)
+                                                }
+                                                disabled={
+                                                    submitting || pristine
+                                                }
+                                            >
+                                                {i18n.t('Reset')}
+                                            </Button>
+                                            <Button
+                                                primary
+                                                type="submit"
+                                                disabled={submitting}
+                                                // How to do?
+                                                // onClick={toggleModal}
+                                            >
+                                                {i18n.t('Save')}
+                                            </Button>
+                                        </ButtonStrip>
+                                    </ModalActions>
+                                </form>
+                            )
+                        }}
+                    </Form>
+                </Modal>
             )}
             <style jsx>{styles}</style>
         </div>
