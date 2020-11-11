ccfa7b4adb0bba0d2faed34feb20d14354ee196f
Kai Vandivier <kpvandivier@gmail.com>
2020-11-11 14:21:25 -0800

    feat: use table layout and discreet dialog

--- src/pages/tables/edit-table-template/HighlightingEditor.js
+++ src/pages/tables/edit-table-template/HighlightingEditor.js
@@ -5,15 +5,23 @@ import {
     ModalTitle,
     ModalContent,
     ModalActions,
+    Table,
+    TableHead,
+    TableRowHead,
+    TableCellHead,
+    TableBody,
+    TableRow,
+    TableCell,
     ReactFinalForm,
     hasValue,
     number,
     composeValidators,
 } from '@dhis2/ui'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import i18n from '../../../locales'
 import { UPDATE_TABLE } from '../../../reducers/tableReducer'
 import styles from './styles/HighlightingEditor.style'
+import utils from '../../../styles/utils.module.css'
 import { useTableDispatch, useTableState } from '../../../context/tableContext'
 
 const defaultIntervals = [
@@ -46,6 +54,126 @@ function betweenNeighbors(key, idx) {
 
         return tooHigh ? tooHighMessage : tooLow ? tooLowMessage : undefined
     }
+}
+
+function HighlightingEditorDialog({ open, toggle, onSave }) {
+    const table = useTableState()
+    if (!open) return null
+
+    const getTableRows = () => {
+        const validateField = idx =>
+            composeValidators(
+                hasValue,
+                number,
+                betweenNeighbors('lowerBounds', idx)
+            )
+
+        return table.highlightingIntervals.map((interval, idx, arr) => (
+            <TableRow key={idx}>
+                <TableCell>
+                    {arr.length === 1 ? (
+                        'All values'
+                    ) : idx === arr.length - 1 ? (
+                        <em className="no-lb">{i18n.t('No lower bound')}</em>
+                    ) : (
+                        <div className="field-container">
+                            <Field
+                                dense
+                                initialFocus={idx === 0}
+                                name={`lowerBounds[${idx}]`}
+                                component={InputFieldFF}
+                                validate={validateField(idx)}
+                                initialValue={String(interval.lowerBound)}
+                            />
+                        </div>
+                    )}
+                </TableCell>
+                <TableCell>
+                    <div
+                        className="color-swatch"
+                        style={{
+                            backgroundColor: interval.color,
+                        }}
+                    />
+                </TableCell>
+                <style jsx>{`
+                    .no-lb {
+                        padding-left: 0.5rem;
+                    }
+                    .field-container {
+                        max-width: 200px;
+                        margin-right: auto;
+                    }
+                `}</style>
+                <style jsx>{styles}</style>
+            </TableRow>
+        ))
+    }
+
+    return (
+        <Modal small onClose={toggle}>
+            <ModalTitle>{i18n.t('Edit Highlighting Intervals')}</ModalTitle>
+            <Form onSubmit={onSave}>
+                {({
+                    handleSubmit,
+                    form,
+                    submitting,
+                    pristine,
+                    values,
+                    initialValues,
+                }) => (
+                    <form onSubmit={handleSubmit}>
+                        <ModalContent>
+                            <Table
+                                suppressZebraStriping
+                                className={utils.noBorder}
+                            >
+                                <TableHead>
+                                    <TableRowHead>
+                                        <TableCellHead>
+                                            {i18n.t('Lower bound of interval')}
+                                        </TableCellHead>
+                                        <TableCellHead
+                                            style={{ width: 'max-content' }}
+                                        >
+                                            {i18n.t('Color')}
+                                        </TableCellHead>
+                                    </TableRowHead>
+                                </TableHead>
+                                <TableBody>{getTableRows(values)}</TableBody>
+                            </Table>
+                        </ModalContent>
+                        <ModalActions>
+                            <ButtonStrip end>
+                                <Button onClick={toggle}>
+                                    {i18n.t('Cancel')}
+                                </Button>
+                                <Button
+                                    onClick={() => form.reset(initialValues)}
+                                    disabled={submitting || pristine}
+                                >
+                                    {i18n.t('Reset')}
+                                </Button>
+                                <Button
+                                    primary
+                                    type="submit"
+                                    disabled={submitting}
+                                >
+                                    {i18n.t('Save')}
+                                </Button>
+                            </ButtonStrip>
+                        </ModalActions>
+                    </form>
+                )}
+            </Form>
+        </Modal>
+    )
+}
+
+HighlightingEditorDialog.propTypes = {
+    open: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired,
+    onSave: PropTypes.func.isRequired,
 }
 
 export function HighlightingEditor() {
@@ -87,53 +215,6 @@ export function HighlightingEditor() {
         })
     }
 
-    function getIntervalEditors(values) {
-        const validateField = idx =>
-            composeValidators(
-                hasValue,
-                number,
-                betweenNeighbors('lowerBounds', idx)
-            )
-
-        return table.highlightingIntervals.map((interval, idx, arr) => (
-            <div className="interval-container" key={idx}>
-                <div
-                    className="color-swatch"
-                    style={{
-                        backgroundColor: interval.color,
-                    }}
-                />
-                {arr.length === 1 ? (
-                    <span>All values</span>
-                ) : idx === arr.length - 1 ? (
-                    <span>
-                        {i18n.t('Value {{-lt}} {{value}}', {
-                            lt: '\x3C',
-                            value: values.lowerBounds
-                                ? values.lowerBounds[idx - 1]
-                                : '',
-                        })}
-                    </span>
-                ) : (
-                    <>
-                        <span>
-                            {i18n.t('Value {{-gte}}', { gte: '\u2265' })}
-                        </span>
-                        <Field
-                            dense
-                            initialFocus={idx === 0}
-                            name={`lowerBounds[${idx}]`}
-                            component={InputFieldFF}
-                            validate={validateField(idx)}
-                            initialValue={String(interval.lowerBound)}
-                        />
-                    </>
-                )}
-                <style jsx>{styles}</style>
-            </div>
-        ))
-    }
-
     return (
         <div>
             <div className="switch-container">
@@ -150,51 +231,11 @@ export function HighlightingEditor() {
             >
                 {i18n.t('Configure')}
             </Button>
-            {modalIsOpen && (
-                <Modal onClose={toggleModal}>
-                    <ModalTitle>
-                        {i18n.t('Edit Highlighting Intervals')}
-                    </ModalTitle>
-                    <Form onSubmit={onSubmit}>
-                        {({
-                            handleSubmit,
-                            form,
-                            submitting,
-                            pristine,
-                            values,
-                            initialValues,
-                        }) => (
-                            <form onSubmit={handleSubmit}>
-                                <ModalContent>
-                                    {getIntervalEditors(values)}
-                                </ModalContent>
-                                <ModalActions>
-                                    <ButtonStrip end>
-                                        <Button onClick={toggleModal}>
-                                            {i18n.t('Cancel')}
-                                        </Button>
-                                        <Button
-                                            onClick={() =>
-                                                form.reset(initialValues)
-                                            }
-                                            disabled={submitting || pristine}
-                                        >
-                                            {i18n.t('Reset')}
-                                        </Button>
-                                        <Button
-                                            primary
-                                            type="submit"
-                                            disabled={submitting}
-                                        >
-                                            {i18n.t('Save')}
-                                        </Button>
-                                    </ButtonStrip>
-                                </ModalActions>
-                            </form>
-                        )}
-                    </Form>
-                </Modal>
-            )}
+            <HighlightingEditorDialog
+                open={modalIsOpen}
+                toggle={toggleModal}
+                onSave={onSubmit}
+            />
             <style jsx>{styles}</style>
         </div>
     )
