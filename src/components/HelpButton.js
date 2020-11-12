7370d5de78daf9262a8cd0e0a99400fc2a159a5b
Kai Vandivier <kpvandivier@gmail.com>
2020-11-12 15:43:13 -0800

    refactor: tooltip instead of button

--- src/components/HelpButton.js
+++ src/components/HelpButton.js
@@ -1,32 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { colors } from '@dhis2/ui'
+import { Tooltip, colors } from '@dhis2/ui'
 import i18n from '../locales'
-import IconTooltipButton from './IconTooltipButton'
 import { HELP } from '../modules/paths'
+import Icon from './Icon'
 
 function HelpButton({ subsection }) {
     if (subsection && subsection[0] !== '#')
         console.warn('subsection prop should start with "#"!')
 
     return (
-        <a
-            href={`${HELP}${subsection}`}
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            <IconTooltipButton
-                tooltip={i18n.t('Help')}
-                icon="help"
-                color={colors.blue700}
-                size="24px"
-            />
-            <style jsx>{`
-                a {
-                    text-decoration: none;
-                }
-            `}</style>
-        </a>
+        <Tooltip content={i18n.t('Help')} placement="bottom">
+            <a
+                href={`${HELP}${subsection}`}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <Icon name="help" color={colors.blue700} />
+                <style jsx>{`
+                    a {
+                        text-decoration: none;
+                    }
+                `}</style>
+            </a>
+        </Tooltip>
     )
 }
 
