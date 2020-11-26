c26abf843e1de186d2aafd59cda79b80f201551a
Kai Vandivier <kpvandivier@gmail.com>
2020-11-25 23:02:10 -0800

    feat: add function and button to create demo table

--- src/pages/tables/saved-table-templates/CreateNewTableTemplate.js
+++ src/pages/tables/saved-table-templates/CreateNewTableTemplate.js
@@ -5,8 +5,9 @@ import { Button } from '@dhis2/ui'
 import InputDialog from '../../../components/InputDialog'
 import Icon from '../../../components/Icon'
 
-export function CreateNewTableTemplate({ createNew }) {
+export function CreateNewTableTemplate({ createNew, demo }) {
     const [modalOpen, setModalOpen] = useState(false)
+    const toggleModal = () => setModalOpen(state => !state)
 
     function onCreateNew(inputText) {
         setModalOpen(false)
@@ -15,16 +16,26 @@ export function CreateNewTableTemplate({ createNew }) {
 
     return (
         <div>
-            <Button
-                icon={<Icon name="add" color="white" />}
-                onClick={() => setModalOpen(true)}
-                primary
-            >
-                {i18n.t('Create new')}
-            </Button>
+            {demo ? (
+                <Button icon={<Icon name="add" />} onClick={toggleModal}>
+                    {i18n.t('Create demo table')}
+                </Button>
+            ) : (
+                <Button
+                    icon={<Icon name="add" color="white" />}
+                    onClick={() => setModalOpen(true)}
+                    primary
+                >
+                    {i18n.t('Create new')}
+                </Button>
+            )}
             {modalOpen && (
                 <InputDialog
-                    title={i18n.t('Create new table template')}
+                    title={
+                        demo
+                            ? i18n.t('Create demo table template')
+                            : i18n.t('Create new table template')
+                    }
                     inputLabel={i18n.t('Table name')}
                     inputPlaceholder={i18n.t('Enter a name')}
                     initialValue={''}
@@ -39,6 +50,7 @@ export function CreateNewTableTemplate({ createNew }) {
 
 CreateNewTableTemplate.propTypes = {
     createNew: PropTypes.func.isRequired,
+    demo: PropTypes.bool,
 }
 
 export default CreateNewTableTemplate
