b98c6af38ffc736ea1f1b9e6f7c17e6ee2ccdb15
Kai Vandivier <kpvandivier@gmail.com>
2020-12-14 15:10:52 -0800

    feat: add superscript footnote ids (rough)

--- src/pages/tables/generated-table/CellData.js
+++ src/pages/tables/generated-table/CellData.js
@@ -3,6 +3,7 @@ import { CircularLoader, Tooltip } from '@dhis2/ui'
 import { useDataQuery } from '@dhis2/app-runtime'
 import PropTypes from 'prop-types'
 import i18n from '../../../locales'
+import { useFootnotes } from '../../../context/footnotesContext'
 
 const ANALYTICS_QUERY = {
     result: {
@@ -25,6 +26,8 @@ function getSelectedNames(selectedItems) {
 
 function CellData({ cell, selectedOrgUnits, selectedPeriods, onLoad }) {
     if (!cell.data.item) return null
+
+    const { orgUnitFootnotes, periodFootnotes } = useFootnotes()
 
     const queryVars = {
         dxId: cell.data.item.id,
@@ -64,6 +67,48 @@ function CellData({ cell, selectedOrgUnits, selectedPeriods, onLoad }) {
         `
     }
 
+    function getFootnotes() {
+        const footnotes = []
+        // If ou, add sup
+        if (cell.data.orgUnits.length > 0) {
+            // get key as list of ids
+            const key = getSelectedIds(cell.data.orgUnits)
+            // check map for existing footnote
+            let footnote = orgUnitFootnotes.get(key)
+            if (footnote === undefined) {
+                footnote = {
+                    id: `ou${orgUnitFootnotes.size + 1}`,
+                    description: getSelectedNames(cell.data.orgUnits),
+                }
+                orgUnitFootnotes.set(key, footnote)
+            }
+            footnotes.push(footnote)
+        }
+        // If pe, add sup
+        if (cell.data.periods.length > 0) {
+            // get key as list of ids
+            const key = getSelectedIds(cell.data.periods)
+            // check map for existing footnote
+            let footnote = periodFootnotes.get(key)
+            if (footnote === undefined) {
+                footnote = {
+                    id: `p${periodFootnotes.size + 1}`,
+                    description: getSelectedNames(cell.data.periods),
+                }
+                periodFootnotes.set(key, footnote)
+            }
+            footnotes.push(footnote)
+        }
+        if (footnotes.length === 0) return null
+        // Add commma between
+        return (
+            <span>
+                {' '}
+                <sup>{footnotes.map(({ id }) => id).join(', ')}</sup>
+            </span>
+        )
+    }
+
     if (loading) return <CircularLoader small />
     if (error) {
         console.error(error)
@@ -78,6 +123,7 @@ function CellData({ cell, selectedOrgUnits, selectedPeriods, onLoad }) {
                     {/* If OU, add ou footnote */}
                     {/* If PE, add pe footnote */}
                     {/* Add comma between if there are both */}
+                    {getFootnotes()}
                 </span>
             )}
         </Tooltip>
