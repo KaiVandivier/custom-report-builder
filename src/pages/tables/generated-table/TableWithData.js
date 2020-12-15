9ccd261638017cd0149c43a6912d02879c916feb
Kai Vandivier <kpvandivier@gmail.com>
2020-12-15 00:37:29 -0800

    feat: populate footnotes in TableWithData

--- src/pages/tables/generated-table/TableWithData.js
+++ src/pages/tables/generated-table/TableWithData.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import {
     Table,
@@ -13,9 +13,61 @@ import i18n from '../../../locales'
 import styles from './styles/TableWithData.styles'
 import { GeneratedTableCell } from './GeneratedTableCell'
 import { useTableState } from '../../../context/tableContext'
+import { useFootnotes } from '../../../context/footnotesContext'
+import Footnotes from './Footnotes'
+
+function getSelectedIds(selectedItems) {
+    return selectedItems.map(({ id }) => id).join(';')
+}
 
 function getSelectedNames(selectedItems) {
     return selectedItems.map(({ name }) => name).join(', ')
+}
+
+function populateFootnotes(table, footnotes) {
+    const { setOrgUnitFootnotes, setPeriodFootnotes } = footnotes
+    const orgUnitFootnotes = new Map()
+    const periodFootnotes = new Map()
+
+    // For each cell, add footnotes for any unique org unit(s) or period(s)
+    table.rows.forEach(row => {
+        row.cells.forEach(cell => {
+            if (cell.data.orgUnits.length > 0) {
+                // get key as list of ids
+                const key = getSelectedIds(cell.data.orgUnits)
+
+                // check map for existing footnote; add if absent
+                if (orgUnitFootnotes.get(key) === undefined) {
+                    const newFootnote = {
+                        id: `ou${orgUnitFootnotes.size + 1}`,
+                        description: getSelectedNames(cell.data.orgUnits),
+                    }
+                    // TODO: replace with map.set(...)
+                    // addOrgUnitFootnote(key, newFootnote)
+                    orgUnitFootnotes.set(key, newFootnote)
+                }
+            }
+
+            if (cell.data.periods.length > 0) {
+                // get key as list of ids
+                const key = getSelectedIds(cell.data.periods)
+
+                // check map for existing footnote; add if absent
+                if (periodFootnotes.get(key) === undefined) {
+                    const newFootnote = {
+                        id: `p${periodFootnotes.size + 1}`,
+                        description: getSelectedNames(cell.data.periods),
+                    }
+
+                    // addPeriodFootnote(key, newFootnote)
+                    periodFootnotes.set(key, newFootnote)
+                }
+            }
+        })
+    })
+
+    setOrgUnitFootnotes(orgUnitFootnotes)
+    setPeriodFootnotes(periodFootnotes)
 }
 
 export function TableWithData({
@@ -24,6 +76,11 @@ export function TableWithData({
     selectedPeriods,
 }) {
     const table = useTableState()
+    const footnotes = useFootnotes()
+
+    useEffect(() => {
+        populateFootnotes(table, footnotes)
+    }, [])
 
     if (periodParamNeeded && !selectedPeriods.length)
         return <p>Waiting for parameters...</p>
@@ -62,6 +119,7 @@ export function TableWithData({
     return (
         <>
             <h2 className="title">{table.name}</h2>
+
             {selectedOrgUnits.length ? (
                 <p>
                     {i18n.t('Organisation Unit{{s}} - {{ou}}', {
@@ -70,6 +128,7 @@ export function TableWithData({
                     })}
                 </p>
             ) : null}
+
             {selectedPeriods.length ? (
                 <p>
                     {i18n.t('Period{{s}} - {{pe}}', {
@@ -78,14 +137,19 @@ export function TableWithData({
                     })}
                 </p>
             ) : null}
+
             <p>
                 {i18n.t('Date - ')}
                 {new Date().toLocaleDateString()}
             </p>
+
             <Table>
                 <TableHead>{tableHeader()}</TableHead>
                 <TableBody>{tableBody()}</TableBody>
             </Table>
+
+            <Footnotes />
+
             <style jsx>{styles}</style>
         </>
     )
