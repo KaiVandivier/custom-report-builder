be53f561eda7f7046eb4a138c4171f0186acf283
Kai Vandivier <kpvandivier@gmail.com>
2021-01-11 20:00:16 -0800

    chore: add test todos

--- src/modules/__tests__/exampleTable.spec.js
+++ src/modules/__tests__/exampleTable.spec.js
@@ -190,7 +190,15 @@ it('correctly produces a set of reducer actions for the given table', () => {
     expect(actions).toEqual(expected)
 })
 
-it('correctly produces reducer actions for column actions', () => {
-    // TODO
-    // const actions = getColumnActions(exampleTable, testData)
+describe('getting column actions', () => {
+    it('correctly produces reducer actions for column actions', () => {
+        // TODO
+        // const actions = getColumnActions(exampleTable, testData)
+    })
+
+    it.todo('correctly handles data items < 4 and > 0')
+    // by updating the relevant columns and deleting the rest
+
+    it.todo('correctly handles data items = 0')
+    // ...by inserting a column with a descriptive name
 })
